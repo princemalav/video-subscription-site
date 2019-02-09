1aee658d579c2925b6b2b83a69d6285d52665ba8
princemalav <33306779+princemalav@users.noreply.github.com>
2019-02-09 17:08:13 +0530

    added membership type and stripe for membership type manage

---
+++ memberships/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Membership , UserMembership , Subscription
+
+# Register your models here.
+admin.site.register(Subscription)
+admin.site.register(Membership)
+admin.site.register(UserMembership)
