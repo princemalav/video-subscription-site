fddb02eb417f015d36dfd1de2211e01bd38aff07
princemalav <33306779+princemalav@users.noreply.github.com>
2019-02-12 01:17:05 +0530

    Added something new

--- memberships/views.py
+++ memberships/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Membership , UserMembership
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from .models import Membership , UserMembership , Subscription
 from django.views.generic import ListView
 # Create your views here.
 
@@ -9,8 +11,32 @@ def get_user_membership(request):
         return user_membership_qs.first()
     return None
 
+def get_user_subscription(request):
+    user_subscription_qs = Subscription.objects.filter(
+                        user_membership=get_user_membership(request))
+    if user_subscription_qs.exists():
+        user_subscription = user_subscription_qs.first()
+        return user_subscription
+    return None
+
+
 class MemberSelectView(ListView):
      model = Membership
+
+     def post(self,request,**kwargs):
+         selected_membership_type = request.POST.get('membership_type')
+         user_membership = get_user_membership(request)
+         user_subscription = get_user_subscription(request)
+
+         selected_membership_qs = Membership.objects.filter(membership_type=selected_membership_type)
+
+
+         if selected_membership_qs.exists():
+             selected_membership = selected_membership_qs.first()
+         request.session['selected_membership_type'] = selected_membership.membership_type
+         return HttpResponseRedirect(reverse('memberships:payment'))
+
+
      def get_context_data(self,*args,**kwargs):
          context = super().get_context_data(**kwargs)
          current_memberhip = get_user_membership(self.request)
